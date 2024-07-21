a232b28e38df615748fa3682457c6011276987c5
devanshugahlot <devanshugahlot79@gmail.com>
2024-07-21 21:57:16 +0530

    frist

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./componets/Home";
+import DrawerAppBar from "./componets/DrawerAppBar";
+
+function App() {
+  return (
+    <>
+      <DrawerAppBar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/Home.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import "./home.css";
+const Home = () => {
+  return (
+    <div className="maincont">
+      <div className="mainbackground">
+        <div className="main-heading">
+          <h1>
+            BHAVYA RAJPUT <br /> TRADING
+          </h1>
+        </div>
+      </div>
+      <div className="second-div">
+        <div className="second-text-div">
+          <p>
+            Our vision is to increase the freedom of money globally. We believe
+            that by spreading this freedom, we can significantly improve lives
+            around the world. Our mission is to provide the core infrastructure
+            services for organizing the world's crypto.
+          </p>
+        </div>
+        <div className="second-img">
+          <img
+            src="https://lh4.googleusercontent.com/gsgA3ix9nVbmxQOQouucLTgJADNzEPfvtMAzBQrZFIQHS3rSFK9YZas1yqPJWOFXlyu03hrXUGLH6AxwEnadvCQ=w1280"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="third-div">
+        <p class="responsive-text">
+          Our mission is to create more economic freedom in the world We define
+          economic freedom as the ability to make choices with respect to one's
+          personal resources, unencumbered by trusted third parties or borders
+          or lack of access.
+        </p>
+      </div>
+
+      <div className="fourth">
+        <section className="about-us">
+          <h1>ABOUT US</h1>
+          <p>
+            Our mission is to help bring financial services into the 21st
+            century by creating new products for different types of traders. We
+            want our name to be the first that comes to mind when anybody thinks
+            about online trading. Every project we work on will be in service of
+            our clients and we will continue to improve our offering to
+            accommodate their needs. We will make a significant contribution to
+            our industry while maintaining a standard of excellence throughout
+            the company and everything we produce.
+          </p>
+        </section>
+        <section className="placement">
+          <img
+            src="https://lh4.googleusercontent.com/jrc7Phi24MrUgYUOXGofzotQ17ZZZ1p5HWTa2pcUTN6jzMI52NIB6Ng1IB24aXrCDRvGKNYszOxk1QtAVoDouSO82swZr4vYjKbwDStM9EFxi3uBqfBnLlyD8fpCBY8RdA=w1280"
+            alt="I am Placed By DYPCET"
+          />
+        </section>
+      </div>
+
+      <hr />
+
+      <div className="fifth">
+        <div className="fifth-text">
+          <h2>PARTNERS</h2>
+        </div>
+        <div className="partners-img">
+          <img
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ43-j8Lq-JyCTf96qvCOWnZDDW6VzeTZcB1lwEQ9Pe-EE6VX2P"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQuO_oaotrr1p33gFQ3g7ScHDVJS-0Ag0np28S4xuFW7F_kENXr"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRCIVi9E54VBB3ToR1rhhO4NrPoR0pYGr7t_JlGWBTQPeMAGUrk"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj-SmQkaZLRoFfqIR_QEs5uDOmrl7U9pmNFhPcnXYxUqiFa2C2"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div style={{ marginTop: "150px" }} className="fifth">
+        <div className="fifth-text">
+          <h2>TRADING CERTIFICATE</h2>
+        </div>
+        <div className="partners-img">
+          <img
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ43-j8Lq-JyCTf96qvCOWnZDDW6VzeTZcB1lwEQ9Pe-EE6VX2P"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQuO_oaotrr1p33gFQ3g7ScHDVJS-0Ag0np28S4xuFW7F_kENXr"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRCIVi9E54VBB3ToR1rhhO4NrPoR0pYGr7t_JlGWBTQPeMAGUrk"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj-SmQkaZLRoFfqIR_QEs5uDOmrl7U9pmNFhPcnXYxUqiFa2C2"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="sixth">
+        <div className="six-div">
+          <div className="imgdiv">
+            <img
+              src="https://lh6.googleusercontent.com/DNVjkJYpx318upHIT9lQaUgLeO4Le1bMdIKp5PRSzae_bxJb9M-fHlBu6WV3sojtqmAz5o6aTUVIKJhtjEb58tskuk8qeJqXux5x7Xsny9kjxDUFpcPQeeHe959ntlEYGQ=w1280"
+              alt=""
+            />
+          </div>
+          <div className="tetxsix">
+            <p>
+              The SEBI-Registered Investment Advisor meaning is a professional
+              authorised and registered with the Securities and Exchange Board
+              of India (SEBI) to provide financial advisory services to clients.
+              A SEBI-Registered Investment Advisor must comply with the code of
+              conduct, disclosure norms, and minimum qualifications and
+              experience requirements set by SEBI. The primary objective of a
+              SEBI-Registered Investment Advisor is to offer unbiased and
+              personalised investment advice to clients based on their financial
+              goals, risk profile, and investment preferences. Investors can
+              receive trustworthy and transparent advice from a SEBI-Registered
+              Investment Advisor.
+            </p>
+          </div>
+        </div>
+          </div>
+          
+          <div className="contact-container">
+      <div className="contact-info">
+        <h2>Contact Us</h2>
+        <div className="contact-item">
+          <h3>WhatsApp Business Number:</h3>
+          <p><a href="tel:+917439165548">7439165548</a></p>
+        </div>
+        <div className="contact-item">
+          <h3>Office Address:</h3>
+          <p>No. G 4 & 5, Ground Floor, No.12</p>
+          <p>Richmond Road, Bangalore - 560025</p>
+          <p>Near HDFC Bank & Petrol Bunk</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Home;
